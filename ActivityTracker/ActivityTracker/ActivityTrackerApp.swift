affc8c02e617f6809a550d222d967f93941bf89e
Oki <biek.syaoki@gmail.com>
2025-12-03 10:23:24 +0700

    Feat: Init project

---
+++ ActivityTracker/ActivityTracker/ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTrackerApp.swift
+//  ActivityTracker
+//
+//  Created by Syaoki Biek on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
